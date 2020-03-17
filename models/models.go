c568559c83794203400728a373c137ff0774a7d1
Anton Ivlev <anton@codesigned.co.uk>
2020-03-17 17:33:35 +0000

    Separate base fields into a separate struct

--- models/models.go
+++ models/models.go
@@ -1,22 +1,7 @@
 package models
 
-import (
-	"github.com/google/uuid"
-	"github.com/graph-gophers/graphql-go"
-	"github.com/jinzhu/gorm"
-)
-
 type Link struct {
-	ID          graphql.ID `gorm:"primary_key"`
+	Base
 	Description string
 	URL         string
-}
-
-// BeforeCreate will set a UUID rather than numeric ID.
-func (link *Link) BeforeCreate(scope *gorm.Scope) error {
-	uuid, err := uuid.NewUUID()
-	if err != nil {
-		return err
-	}
-	return scope.SetColumn("ID", uuid.String())
 }
